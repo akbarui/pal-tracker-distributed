2b76473b7e1e4bab7ab5bd52ccba9e7ec3ef6f27
akbarui <amitkumar.barui@cognizant.com>
2020-03-14 02:41:03 +0530

    OAuth security

--- platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/App.java
+++ platform-services/oauth-server/src/main/java/io/pivotal/pal/tracker/oauthserver/App.java
@@ -1,5 +1,10 @@
 package io.pivotal.pal.tracker.oauthserver;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
 @SpringBootApplication
 public class App extends AuthorizationServerConfigurerAdapter {
 
